3842a7f916b99c905ccb5c1df67c6da31ad4f04e
kunteinzo <87855489+kunteinzo@users.noreply.github.com>
2023-05-28 10:29:09 +0000

    Make all evaloate work in all pages as possible.

--- index.php
+++ index.php
@@ -1,67 +1,54 @@
-<?php
+ဘ<?php
 require 'vendor/autoload.php';
-
-use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\Request;
+require 'utils.php';
 
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: GET");
-#header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
 header("Access-Control-Max-Age: 3600");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 
-$uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
-echo $uri;
 /*
+$uri = parse_url($_SERVER['REQUEST_URI'],PHP_URL_PATH);
+$uri = explode('/',$uri);
+
 if($_SERVER['REQUEST_METHOD']=='GET'){
   echo 'This is get';
 }*/
 
-function request(string $url): DOMXPath {
-  $client = new Client();
-  $headers = [
-  'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'];
-  $request = new Request('GET', 'https://theync.com/', $headers);
-  $res = $client->sendAsync($request)->wait();
-  //echo $res->getBody();
-  $htmlString = (string) $res->getBody();
-  //add this line to suppress any warnings
-  libxml_use_internal_errors(true);
-  $doc = new DOMDocument();
-  $doc->loadHTML($htmlString);
-  return new DOMXPath($doc);
-}
+$uri = htmlspecialchars($_GET['url']);
 
-$xpath = request('https://www.theync.com/');
+$xpath = request($uri);
 
-$imgs = $xpath->evaluate('//div[@class="today-top"]//div[@class="inner-block"]//a//span[@class="image"]//img/@src');
+$imgs = ext($xpath, '//div[@class="content-block"]//div[@class="inner-block"]//a//span[@class="image"]//img/@src');
 
-$titles = $xpath->evaluate('//div[@class="today-top"]//div[@class="inner-block"]//a//span[@class="title"]');
+$golds = ext($xpath, '//div[@class="content-block"]//div[@class="inner-block"]//a//span[@class="item-info"]//span[@class="border-gold"]');
 
-$users = $xpath->evaluate('//div[@class="today-top"]//div[@class="inner-block"]//span[@class="user"]//a');
+$titles = $xpath->evaluate('//div[@class="content-block"]//div[@class="inner-block"]//a//span[@class="item-info"]//span[@class="title"]');
 
-$links = $xpath->evaluate('//div[@class="today-top"]//div[@class="inner-block"]//a/@href');
+$users = $xpath->evaluate('//div[@class="content-block"]//div[@class="inner-block"]//span[@class="user"]//a');
 
-$upeds = $xpath->evaluate('//div[@class="today-top"]//div[@class="inner-block"]//span[@class="item-info"]//span[@class="added left"]//span[@class="count"]');
+$links = $xpath->evaluate('//div[@class="content-block"]//div[@class="inner-block"]//a[@data-title]');
+
+$upeds = $xpath->evaluate('//div[@class="content-block"]//div[@class="inner-block"]//span[@class="item-info"]//span[@class="added left"]//span[@class="count"]');
 
 $fulldb = [];
+
+function read(DOMNodeList $nodelist,int $pos):string{
+  $v1 = $nodelist->item($pos);
+  return ($v1!=null)? $v1->textContent : 'Null';
+}
 
 for ($i = 0; $i < count($imgs);$i++) {
   $fulldb[$i]["img"] = $imgs->item($i)->textContent;
   $fulldb[$i]["title"] = $titles->item($i)->textContent;
-  $fulldb[$i]["user"] = $users->item($i)->textContent;
-  $tid = $users->item($i)->getAttribute('href');
-  $fulldb[$i]['userid'] = (str_contains($tid,'theync')) ?substr($tid,20) : 'null';
+  $fulldb[$i]["user"] = /*$users->item($i)->textContent;*/read($users,$i);
+  $fulldb[$i]['id_gold'] = $golds->item($i)->textContent;
+  #$tid = $users->item($i)->getAttribute('href');
+  #$fulldb[$i]['userid'] = (str_contains($tid,'theync')) ?substr($tid,20) : 'null';
   $fulldb[$i]['uploaded_date'] = $upeds->item($i)->textContent;
-  $fulldb[$i]['link'] = $links->item($i)->textContent;
+  $fulldb[$i]['link'] = $links->item($i)->getAttribute('href');
 }
 
-#add to github
-//echo '<img src=\"'.$fulldb[5]["img"].'" />'
 echo json_encode($fulldb);
-/*
-$src = $xpath->evaluate("string(//head//title)");
-print str_contains(strtolower($src),'theync');*/
-
 ?>
--- utils.php
+++ utils.php
@@ -1,3 +1,23 @@
 <?php
+require 'vendor/autoload.php';
+
+function request(string $url): DOMXPath{
+  $client = new GuzzleHttp\Client();
+  $headers = [
+  'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36'];
+  $request = new GuzzleHttp\Psr7\Request('GET', $url, $headers);
+  $res = $client->sendAsync($request)->wait();
+  //echo $res->getBody();
+  $htmlString = (string) $res->getBody();
+  //add this line to suppress any warnings
+  libxml_use_internal_errors(true);
+  $doc = new DOMDocument();
+  $doc->loadHTML($htmlString);
+  return new DOMXPath($doc);
+}
+
+function ext(DOMXPath $xpath,string $reg): DOMNodeList{
+  return $xpath->evaluate($reg);
+}
 
 ?>
