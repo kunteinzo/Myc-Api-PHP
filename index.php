a26976adf7b81b49f695caec10807ffda9e870f8
kunteinzo <87855489+kunteinzo@users.noreply.github.com>
2023-05-31 03:18:18 +0000

    again and again

--- index.php
+++ index.php
@@ -15,69 +15,29 @@ $url = 'https://theync.com';
 // Re-index the array
 $uri = explode('/',$uri);
 
+if (isset($_GET['url'])){
+  getMain($_GET['url']);
+  exit(0);
+}
+
 if ($uri[1]=='search'){
   $url = $url."/search/$uri[2]/page".(($uri[3]!=null)?$uri[3]:1).".html";
+  getMain($url);
 } else if ($uri[1]=='latest'){
   $url = $url."/most-recent/";
-}
+  getMain($url);
+} else if ($uri[1]=='channels'){
+  $url = $url.'/channels';
+  $x = request($url);
+  $titles = ext($x,'//div[@class="content-block"]//div[@class="inner-block"]//span[@class="title"]');
 
-/*
-if($_SERVER['REQUEST_METHOD']=='GET'){
-  echo 'This is get';
-}*/
-
-
-if (isset($_GET['search'])){
-  $kw = htmlspecialchars($_GET['search']);
-  $turl = "/search/$kw/page1.html";
-  $tpage;
-  if (isset($_GET['page'])){
-    $tpage = htmlspecialchars($_GET['page']);
-    $turl = "/search/$kw"."/page$tpage.html";
+  $links = ext($x,'//div[@class="content-block"]//div[@class="inner-block"]//a//@href');
+  $db = [];
+  for ($i = 0;$i < count($titles);$i++){
+    $db[$i]['title'] = $titles->item($i)->textContent;
+    $db[$i]['link'] = $links->item($i)->textContent;
   }
-  $url = $url.$turl;
+  echo json_encode($db);
 }
 
-$xpath = request($url);
-$db = [];
-
-$pages = ext($xpath, '//div[@id="pages"]//div[@class="pagination-row"]//div[@class="inner-block"]//a/@href');
-
-$db['active'] = (int)ext($xpath, 'string(//div[@id="pages"]//div[@class="pagination-row"]//div[@class="inner-block"]//span[@class="active"])');
-
-for ($i = 0;$i < count($pages);$i++) {
-  $db['page'][$i] = (int)substr($pages->item($i)->textContent,4,-5);
-}
-
-$imgs = ext($xpath, '//div[@class="content-block"]//div[@class="inner-block"]//a//span[@class="image"]//img/@src');
-
-$golds = ext($xpath, '//div[@class="content-block"]//div[@class="inner-block"]//a//span[@class="item-info"]//span[@class="border-gold"]');
-
-$titles = ext($xpath, '//div[@class="content-block"]//div[@class="inner-block"]//a//span[@class="item-info"]//span[@class="title"]');
-
-$users = ext($xpath, '//div[@class="content-block"]//div[@class="inner-block"]//span[@class="user"]//a');
-
-$links = ext($xpath, "//div[@class=\"content-block\"]//div[@class=\"inner-block\"]//a[@data-title]/@href");
-
-$upeds = ext($xpath, '//div[@class="content-block"]//div[@class="inner-block"]//span[@class="item-info"]//span[@class="added left"]//span[@class="count"]');
-
-for ($i = 0; $i < count($imgs);$i++) {
-  $db['data'][$i]["img"] = $imgs->item($i)->textContent;
-  $db['data'][$i]["title"] = $titles->item($i)->textContent;
-  $user = $users->item($i);
-  
-  $db['data'][$i]["user"] = ($user!=null)? $user->textContent:'';
-  
-  $db['data'][$i]['userpath'] = ($user!=null)? $user->getAttribute('href'):'';
-
-  $db['data'][$i]['isexternal'] = str_contains($db['data'][$i]['user'],'theync');
-
-  $db['data'][$i]['isgold'] = ($golds->item($i)!=null);
-  
-  $db['data'][$i]['uploaded_date'] = $upeds->item($i)->textContent;
-  
-  $db['data'][$i]['link'] = ext($xpath, 'string(//div[@class="content-block"]//div[@class="inner-block"]//a[@title="'.$db["data"][$i]["title"].'"]/@href)');
-}
-
-echo json_encode($db);
 ?>
--- utils.php
+++ utils.php
@@ -27,4 +27,48 @@ function upbyuser($link){
   return ext(request($link), 'string(//div[@class="upload-button"]//div[@class="inner-block"]/@href)');
 }
 
+function getMain($url){
+  $xpath = request($url);
+  $db = [];
+
+  $pages = ext($xpath, '//div[@id="pages"]//div[@class="pagination-row"]//div[@class="inner-block"]//a/@href');
+
+  $db['active'] = (int)ext($xpath, 'string(//div[@id="pages"]//div[@class="pagination-row"]//div[@class="inner-block"]//span[@class="active"])');
+
+  for ($i = 0;$i < count($pages);$i++) {
+    $db['page'][$i] = (int)substr($pages->item($i)->textContent,4,-5);
+  }
+
+  $imgs = ext($xpath, '//div[@class="content-block"]//div[@class="inner-block"]//a//span[@class="image"]//img/@src');
+
+  $golds = ext($xpath, '//div[@class="content-block"]//div[@class="inner-block"]//a//span[@class="item-info"]//span[@class="border-gold"]');
+
+  $titles = ext($xpath, '//div[@class="content-block"]//div[@class="inner-block"]//a//span[@class="item-info"]//span[@class="title"]');
+
+  $users = ext($xpath, '//div[@class="content-block"]//div[@class="inner-block"]//span[@class="user"]//a');
+
+  $links = ext($xpath, "//div[@class=\"content-block\"]//div[@class=\"inner-block\"]//a[@data-title]/@href");
+
+  $upeds = ext($xpath, '//div[@class="content-block"]//div[@class="inner-block"]//span[@class="item-info"]//span[@class="added left"]//span[@class="count"]');
+
+  for ($i = 0; $i < count($imgs);$i++) {
+    $db['data'][$i]["img"] = $imgs->item($i)->textContent;
+    $db['data'][$i]["title"] = $titles->item($i)->textContent;
+    $user = $users->item($i);
+  
+    $db['data'][$i]["user"] = ($user!=null)? $user->textContent:'';
+  
+    $db['data'][$i]['userpath'] = ($user!=null)? $user->getAttribute('href'):'';
+
+    $db['data'][$i]['isexternal'] = str_contains($db['data'][$i]['user'],'theync');
+
+    $db['data'][$i]['isgold'] = ($golds->item($i)!=null);
+  
+    $db['data'][$i]['uploaded_date'] = $upeds->item($i)->textContent;
+  
+    $db['data'][$i]['link'] = ext($xpath, 'string(//div[@class="content-block"]//div[@class="inner-block"]//a[@title="'.$db["data"][$i]["title"].'"]/@href)');
+  }
+  echo json_encode($db);
+}
+
 ?>
